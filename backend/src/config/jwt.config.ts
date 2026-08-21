8e7c74ef7bb65ef165169172745f66c302b50e69
ilyes <mingo.huncho@gmail.com>
2026-08-22 00:41:11 +0100

    fixed jwt

--- backend/src/config/jwt.config.ts
+++ backend/src/config/jwt.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET || 'super-secret-key-change-this',
+  secret: process.env.JWT_SECRET ,
   accessExpiration: process.env.JWT_ACCESS_EXPIRATION || '15m',
   refreshExpiration: process.env.JWT_REFRESH_EXPIRATION || '7d',
 }));
